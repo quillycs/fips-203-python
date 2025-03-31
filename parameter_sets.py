e1ef004f62f416b8588357715354c9b5baf0bfd5
LuayB <luaboett@gmail.com>
2025-03-31 16:02:56 +0100

    Leave 512 as the default parameter set

--- parameter_sets.py
+++ parameter_sets.py
@@ -3,7 +3,7 @@
     UNCOMMENT THE PARAMETER SET THAT YOU ARE TESTING.
 '''
 
-# Constants
+# Constants (do not change these)
 n = 256
 q = 3329
 zeta = 17
@@ -20,9 +20,9 @@ k = 3
 eta1 = 2
 eta2 = 2
 du = 10
-dv = 4'''
+dv = 4
 
-'''# ML-KEM-1024
+# ML-KEM-1024
 k = 4
 eta1 = 2
 eta2 = 2
